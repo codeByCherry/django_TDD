deaf5653e26226e74ab1b30a9a66f0449b69c80a
codyByCherry <373466869@qq.com>
2018-09-10 14:48:05 +0800

    页面重构

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
+
+LISTS_URL = 'http://localhost:8000/lists/'
+MAX_SECONDS = 5
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -11,8 +16,28 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_title(self):
-        self.browser.get('http://localhost:8000/lists/')
+        self.browser.get(LISTS_URL)
         self.assertIn('To-Do', self.browser.title)
+
+    def test_store_lists_and_can_retrieve_it_later(self):
+        # 验证文本中的h1 标签的内容
+        self.browser.get(LISTS_URL)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(input_box.get_attribute('placeholder'),
+                         'Enter a to-do item'
+                         )
+
+
+        todo1 = '##test1'
+        input_box.send_keys(todo1)
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        content = self.browser.find_element_by_id('id_list_table').text
+        self.assertIn(todo1, content)
 
 
 if __name__ == '__main__':
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,15 +3,23 @@ from django.urls import resolve
 from .views import home_page
 from django.http import HttpRequest
 
+
 # Create your tests here.
 class HomePageTest(TestCase):
 
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
     # 确认当前'/' 对应的views 函数是 home_page
     def test_root_url_resolve_to_home_page_view(self):
         response = self.client.get('/lists/')
+        response_content = response.content.decode()
+
         self.assertTemplateUsed(response, 'lists/home_page.html')
+        self.assertIn('<title>To-Do lists</title>', response_content)
 
-
+    def test_can_save_a_POST_request(self):
+        pass
