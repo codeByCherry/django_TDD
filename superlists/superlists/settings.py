a95f1ea9352dd12e3d97e7612ac6675b83ef01c6
codyByCherry <373466869@qq.com>
2018-09-11 17:25:27 +0800

    部署失败，修改配置文件

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'lists.apps.ListsConfig',
+    #'lists.apps.ListsConfig',
+    'lists',
 ]
 
 MIDDLEWARE = [
